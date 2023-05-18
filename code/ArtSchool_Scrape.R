13130fdbd952f57b9f660d8174dfc3adae10d462
[Dan Gunn] <dangunn.studio@gmail.com>
2023-05-18 16:56:37 -0400

    Updated subjects and degrees

--- code/ArtSchool_Scrape.R
+++ code/ArtSchool_Scrape.R
@@ -5,7 +5,6 @@
 library(tidyverse)
 library(rvest)
 library(polite)
-library(stringr)
 library(curl)
 library(broom)
 
@@ -16,7 +15,8 @@ info <- scrape(nasad_url) %>%
   html_text2() %>%
   trimws()
 
-school_names <-info
+#table of school names
+school_names <-as_tibble(info)
 
 #part of the url's to add onto IE "?id=I1190"
 nasad_nodes <- scrape(nasad_url) %>%
@@ -26,30 +26,18 @@ nasad_nodes <- scrape(nasad_url) %>%
 
 # https://nasad.arts-accredit.org/directory-lists/accredited-institutions/search/ Add to URL
 nasad_URL_clean <- str_replace(nasad_nodes,"[?]","https://nasad.arts-accredit.org/directory-lists/accredited-institutions/search/?")
-#list of search terms
-str(nasad_URL_clean)
 
+#list of search terms & inspecting
 nasad_URL_tbl <- as_tibble(nasad_URL_clean)
+str(nasad_URL_tbl)
+head(nasad_URL_tbl)
 
 #combined first scrape of list of URLs and school names + export to CSV
 nasad_web_full <- tibble(school_names, nasad_URL_clean)
-write.csv(nasad_web_full, "nasadwebfull.csv", row.names=FALSE)
+write.csv(nasad_web_full, "data/nasadwebfull.csv", row.names=FALSE)
 
+#creating a smaller test set
 nasad_tbl_shrt <- nasad_URL_tbl[1:4,]
-
-
-#trying to perfect the scrape
-# start_html <- bow("https://nasad.arts-accredit.org/directory-lists/accredited-institutions/search/?id=I1190")
-# start_nodes <- scrape(start_html) %>% html_elements(".wpb_wrapper")
-# class(start_nodes)
-
-#use 2nd instance of class= wpb_wrapper
-# name <- start_nodes[2] %>% html_elements("h2") %>% html_text(.)
-# address_url <- start_nodes[2] %>% html_elements("h2 + p") %>% html_text(.)
-# school_type <- start_nodes[2] %>% html_elements("p:nth-child(5)") %>% html_text(.)
-# school_info <- tibble(name=name, address=address_url, type=school_type)
-# degrees <- start_nodes[2] %>% html_elements("p:nth-child(10)") %>% html_text(.)
-
 
 #make into a function to do all of the urls from nasad_url_clean
 # Bulk scrape function
@@ -62,6 +50,7 @@ nasad_f <- function(x) {
 #Scrape function works but produces a list of xml_nodesets
 test <- apply(X= nasad_tbl_shrt, FUN = nasad_f, MARGIN = 1)
 str(test)
+head(test)
 
 #write a function to parse the xml_nodes
 subset_f <- function(x){
@@ -75,40 +64,67 @@ subset_f <- function(x){
 #lapply and convert to table.
 test_info <- lapply(test, subset_f) %>% 
   data.table::rbindlist()
+head(test_info)
 
 #NOTE: degrees are tricky because both the 9th or 10th child of p is the degree listing.
-
 #split out the degree types (4 year, associates, bachelors etc) on periods and colons.
 #use them as columns and then separate the degrees subjects below them.
-
 test_deg_longer <- test_info$degree %>% str_split_fixed(., pattern="\\.", n=Inf) %>% as.data.frame()
 #expanded degrees (subjects still in tow) recombined with schools
-test_info_combined <- cbind(test_info, test_deg_longer) %>% drop(name)
+test_info_combined <- cbind(test_info, test_deg_longer)
+#drop redundant degree column
+test_info_combined <- subset(test_info_combined, select=-degree)
+
 #now pivoting longer
-test_info_pivot1 <- pivot_longer(test_info_combined, cols= starts_with("V"), values_to = "degree_type", names_repair = "minimal", values_drop_na=TRUE)
-#this works, but use a mutate to keep it inside of the dataframe
-test_set<- test_info_pivot1$degree_type %>% str_split_fixed(.,pattern=":|;", n=Inf)
-test_info_pivot1 %>% group_by(name) 
+test_info_pivot1 <- pivot_longer(test_info_combined, 
+                                 cols= starts_with("V"),
+                                 names_to = NULL,
+                                 values_to = "degree_type", 
+                                 names_repair = "minimal", 
+                                 values_drop_na=TRUE)
+#now delete rows with NA or empty 'degree-type's
+test_info_pivot1 <- test_info_pivot1[!(is.na(test_info_pivot1$degree_type) | test_info_pivot1$degree_type==""), ]
+
+#expand degrees to break out subjects into columns_degree.type.2
+test_set <- lapply(test_info_pivot1, function(x) {str_split_fixed(x,pattern=":|;", n=Inf)}) %>% as.data.frame()
+#change column name to degree to avoid the pivot
+colnames(test_set)[which(names(test_set) == 'degree_type.1')] <- 'degree'
+#pivoting to make tidy
+test_set2 <- pivot_longer(test_set, 
+             cols= starts_with("degree_type."), 
+             names_to= NULL,
+             values_to = "subject", 
+             names_repair = "minimal", 
+             values_drop_na=TRUE)
+#removing empty rows
+test_set2 <- test_set2[!(is.na(test_set2$subject) | test_set2$subject ==""), ]
 
 
+#rename address 1 &2 
+#delete "Web.site" from address.1"
+test_set2$address.1 <- str_replace(test_set2$address.1, pattern =".Web Site", "")
+colnames(test_set2)[which(names(test_set2) == 'address.2')] <- 'url'
+#parse address into street address, city, state, zip
+#zip
+test_set2$zip <- str_extract(test_set2$address.1, "[0-9]{5}")
+#state
+test_set2$state <- str_extract(test_set2$address.1, "[A-Z]{2}")
+#delete zip and state
+test_set2$address.1 <- str_replace(test_set2$address.1, pattern =", [A-Z]{2} [0-9]{5}", "")
+#city (use first instance of comma from the end of the string)
+tmp <- as_tibble(str_split_fixed(test_set2$address.1, "[,](?=[^,]*$)" , n=2))
+colnames(tmp)[which(names(tmp) == 'V1')] <- 'streetaddress'
+colnames(tmp)[which(names(tmp) == 'V2')] <- 'city'
 
-#test_set <- lapply(test_info_pivot1, function(x) {str_split_fixed(x,pattern=":|;", n=Inf)}) %>% as.data.frame()
- 
-#now a data frame. rows are from the school. V1, V2, V3 are the degrees. 
-class(test_deg_longer_split)
-pivot_longer(test_deg_longer_split, everything(), cols_vary = "slowest")
-#how to recombining with test_info?
-test_info_expanded <- cbind(test_info, test_deg_longer_split)
+#add back in temp address and delete 'address.1'
+test_set2 <- bind_cols(test_set2, tmp)
+test_set2 <- select(test_set2, -address.1)
+test_set2 <- test_set2 %>% relocate(any_of(c("streetaddress","city","state","zip")), .after=name)
 
-# #test code
-# degrees_longer <- as.data.frame(unlist(str_split(degrees, "\\.")))
-# degrees_longer <- as.data.frame(str_split_fixed(degrees_longer[,1], pattern=":", n=2))
-# degrees_longer <- degrees_longer %>% rename('degrees'='V1', 'subject'='V2')
-# head(degrees_longer)
-# 
-# #expand the subjects
-# degrees_longest <- degrees_longer$subject %>% str_split_fixed( pattern =";", n=30) %>% bind_cols(degrees_longer$degrees,.)
-# head(degrees_longest)
-# degrees_longest <- degrees_longest %>% rename('degrees'='...1', 'subject_1'='...2')
+#parse type into helpful categories ?
+
+#test complete! Now to apply to the whole list.
+
+
 
 
